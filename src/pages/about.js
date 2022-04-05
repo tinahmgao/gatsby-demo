a13001a011f4a3e444dcb11fc7505ed26d8f3914
Tina <tina.hm.gao@gmail.com>
2022-04-06 00:08:29 +1200

    gatsby pages and routes

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum neque
+        suscipit consequatur? Provident nihil amet error, ad incidunt id
+        repellendus eligendi fuga quas, iure animi iusto. Aperiam accusantium
+        asperiores aut.
+      </p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+    </div>
+  )
+}
