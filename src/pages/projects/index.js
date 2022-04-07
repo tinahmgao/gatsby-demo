5d012f75b4c913084e8fec2c2403baadbb4867de
Tina <tina.hm.gao@gmail.com>
2022-04-07 23:04:35 +1200

    optimize multi images

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import { portfolio, projects } from "../../styles/page/_projects.module.scss"
+import Img from "gatsby-image"
 
 export default function Projects({ data }) {
   const allProjects = data.projects.nodes
@@ -16,6 +17,7 @@ export default function Projects({ data }) {
           {allProjects.map(project => (
             <Link to={`/projects/${project.frontmatter.slug}`} key={project.id}>
               <div>
+                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
                 <h3>{project.frontmatter.title}</h3>
                 <p>{project.frontmatter.stack}</p>
               </div>
@@ -40,6 +42,13 @@ export const query = graphql`
           title
           stack
           slug
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
         }
         id
       }
